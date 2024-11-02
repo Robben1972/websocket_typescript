91efaab3db99c40e7b32a43e472f443d48ae9ebd
Shakhobiddin Bozorov <gravitifolz.01@gmail.com>
2024-11-02 14:10:55 +0500

    Something

--- src/app/Components/LoginPage.tsx
+++ src/app/Components/LoginPage.tsx
@@ -1,6 +1,12 @@
-export default function LoginPage() {
+import { ChangeEvent } from "react";
+
+type loginPagePropsType = {
+    inputNameHandler: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function LoginPage({inputNameHandler} : loginPagePropsType) {
     return (<div className="border-2 flex flex-col  gap-4 w-96 p-4">
-        <input className="border-2 p-2" type="text" name="name" placeholder="Enter First Name" />
+        <input className="border-2 p-2" type="text" name="name" placeholder="Enter First Name" onChange={inputNameHandler}/>
         <button className="border-2 px-11 py-3 bg-blue-950 text-white">Log in</button>
     </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 
 import { log } from "console";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { io ,Socket } from "socket.io-client";
 import LoginPage from "./Components/LoginPage";
 import HomePage from "./Components/homePage";
@@ -10,6 +10,10 @@ import HomePage from "./Components/homePage";
 export default function Home() {
   const [socket, setSocket] = useState<Socket>();
   const [name, setName] = useState<string>();
+
+  const inputNameHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  }
 
   useEffect(() => {
     setSocket(io("http://localhost:3001"));
@@ -23,7 +27,7 @@ export default function Home() {
   }, [socket]);
 
   return <div className="grid place-items-center h-dvh">
-    <LoginPage/>
+    <LoginPage inputNameHandler={inputNameHandler}/>
     <HomePage/>
   </div>
 }
